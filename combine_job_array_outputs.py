e52f65cc615231e8ebcf992d4a55906645e38c1a
Vincent Horvath <vhorvath2010@gmail.com>
2023-10-13 16:06:20 -0400

    Add job array combination script

---
+++ combine_job_array_outputs.py
@@ -0,0 +1,32 @@
+import torch
+import sys
+
+# Get file location and number of jobs from cmdline input
+if len(sys.argv) != 3:
+    print("Invalid Arguments!\nUsage: python combine_job_arrray_outputs.py %path_prefix% %n_jobs$")
+    exit()
+
+path_prefix = sys.argv[1]
+n_jobs = int(sys.argv[2])
+
+# Merge the y outputs from each partial graph
+ys = None
+for i in range(n_jobs):
+    graphs = torch.load(path_prefix + str(i) + ".pt")
+    # Handle first case, if ys is none, just set it equal to this graphs ys
+    if xs = None:
+        ys = [graph.y for graph in graphs]
+        continue
+
+    # Otherwise, if a graph.y contains a non -1 value, replace the value in ys
+    ys_update = [graph.y for graph in graphs]
+    for i in range(len(ys)):
+        for j in range(len(ys[i])):
+            if ys[i][j] == -1 and ys_update[i][j] != -1:
+                ys[i][j] = ys_update[i][j]
+
+# Take first set of graphs and update with merged ys, save that
+graphs = torch.load(path_prefix + "0.pt")
+for i in range(len(graphs)):
+    graphs[i].y = ys[i]
+torch.save(graphs, path_prefix + "Merged.pt")
